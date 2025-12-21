f7d87546da43b85aed9dcbfa9436880cc04030d2
Julian <jullys091119@gmail.com>
2025-12-20 23:32:55 -0700

    Se desarrolló la parte de las reacciones que siguen inconclusas  y se insertaron comentarios y quedo inconcluso leerlos todos.

--- listeners/listeners.js
+++ listeners/listeners.js
@@ -1,7 +1,20 @@
+import { addComments} from "../queriesFirebase.js";
 
-export function carruselListeners (btn,container,direction,itemWidth) {
-  btn.addEventListener("click", (e)=> {
+export function carruselListeners(btn, container, direction, itemWidth) {
+  btn.addEventListener("click", (e) => {
     container.scrollLeft += itemWidth * direction
+  })
+}
+
+export function sendComment(
+  iconSend,
+  containerComments,
+  inputComments,
+  idPostComments
+) {
+  iconSend.addEventListener("click", (e) => {
+    containerComments.style.display = "block";
+    addComments(inputComments.value, idPostComments);
   })
 }
 
