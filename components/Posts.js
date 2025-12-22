aad706c8376aa5148fbd7d8293b71138788b0ba2
Julian <jullys091119@gmail.com>
2025-12-21 21:38:04 -0700

    Comentarios y  likes

--- components/Posts.js
+++ components/Posts.js
@@ -1,182 +1,185 @@
-// posts.js
 import { getProjects, getPerfilImages, getComments } from "../queriesFirebase.js";
-import { sendComment } from "../listeners/listeners.js";
+import { sendComment, likePost } from "../listeners/listeners.js";
 
 export default async function Posts() {
-    const containerPosts = document.querySelector(".aside-right");
-    const data = await getProjects();
-    const avatarURL = (await getPerfilImages()).imagePerfil;
+  const containerPosts = document.querySelector(".aside-right");
+  const data = await getProjects();
+  const avatarURL = (await getPerfilImages()).imagePerfil;
 
-    for (const [i, element] of data.entries()) {
-        const { post, inputComments, containerComments, datePostMain } = createPost(element, i, avatarURL);
-        containerPosts.appendChild(post);
+  for (const [i, element] of data.entries()) {
+    const { post, inputComments, containerComments, containerReaction, datePostMain } = createPost(element, i, avatarURL);
+    containerPosts.appendChild(post);
 
-        const comments = await getComments(post.id);
-        renderComments(comments, containerComments, avatarURL);
+    const comments = await getComments(post.id);
+    renderComments(comments, containerComments, avatarURL);
+    updateCommentCount(containerReaction, comments.length);
 
-        sendComment(containerComments, inputComments, post.id);
-        formatDate(datePostMain, element);
-    }
+    const iconSend = post.querySelector(`.icon-send-${i}`);
+    sendComment(iconSend, containerComments, inputComments, post.id);
+
+    const iconThumb = containerReaction.querySelector(".bi-hand-thumbs-up-fill");
+    const spanLike = containerReaction.querySelector(".div-container-thumb span");
+    likePost(iconThumb, spanLike, post.id); // toggle like real con Firestore
+
+    formatDate(datePostMain, element);
+  }
 }
 
-// -----------------------------
-// Helpers
-// -----------------------------
+/* -------------------------
+   Funciones auxiliares
+------------------------- */
+
 function createPost(element, i, avatarURL) {
-    const post = document.createElement("div");
-    post.className = "posts-container";
-    post.id = `post-${i}`;
+  const post = document.createElement("div");
+  post.className = "posts-container";
+  post.id = `post-${i}`;
 
-    // Header
-    const { postDataUser, datePostMain } = createPostHeader(avatarURL);
+  const { postDataUser, datePostMain } = createPostHeader(avatarURL);
 
-    // Description
-    const descriptionPost = document.createElement("p");
-    descriptionPost.textContent = element.descripcion;
-    descriptionPost.className = "description-post";
+  const descriptionContainer = document.createElement("div");
+  descriptionContainer.className = "description-container";
+  const descriptionPost = document.createElement("p");
+  descriptionPost.textContent = element.descripcion;
+  descriptionPost.className = "description-post";
+  descriptionContainer.appendChild(descriptionPost);
 
-    // Image
-    const imageContainerPost = document.createElement("div");
-    imageContainerPost.className = "image-container-posts";
-    const imgPost = document.createElement("img");
-    imgPost.src = element.image;
-    imgPost.className = "img-post";
-    imageContainerPost.appendChild(imgPost);
+  const imageContainerPost = document.createElement("div");
+  imageContainerPost.className = "image-container-posts";
+  const imgPost = document.createElement("img");
+  imgPost.src = element.image;
+  imgPost.className = "img-post";
+  imageContainerPost.appendChild(imgPost);
 
-    // Reactions
-    const containerReaction = document.createElement("div");
-    containerReaction.className = "container-reaction";
-    createReactionBar(containerReaction);
+  const containerReaction = document.createElement("div");
+  containerReaction.className = "container-reaction";
+  createReactionBar(containerReaction);
 
-    // Comments container
-    const containerComments = document.createElement("div");
-    containerComments.className = "container-comments";
+  const containerComments = document.createElement("div");
+  containerComments.className = "container-comments";
 
-    // Input
-    const containerInput = document.createElement("div");
-    containerInput.className = "container-input";
-    createSendIcon(containerInput, i);
+  const containerInput = document.createElement("div");
+  containerInput.className = "container-input";
+  createSendIcon(containerInput, i);
 
-    const imageAvatarInput = document.createElement("img");
-    imageAvatarInput.src = avatarURL;
-    imageAvatarInput.className = "image-avatar";
+  const imageAvatarInput = document.createElement("img");
+  imageAvatarInput.src = avatarURL;
+  imageAvatarInput.className = "image-avatar";
 
-    const inputComments = document.createElement("input");
-    inputComments.type = "text";
-    inputComments.placeholder = "Escribe un comentario";
-    inputComments.className = "input-comment";
+  const inputComments = document.createElement("input");
+  inputComments.type = "text";
+  inputComments.placeholder = "Escribe un comentario";
+  inputComments.className = "input-comment";
 
-    containerInput.append(imageAvatarInput, inputComments);
+  containerInput.append(imageAvatarInput, inputComments);
 
-    // Append all
-    post.append(postDataUser, descriptionPost, imageContainerPost, containerReaction, containerComments, containerInput);
+  post.append(postDataUser, descriptionContainer, imageContainerPost, containerReaction, containerComments, containerInput);
 
-    return { post, inputComments, containerComments, datePostMain };
+  return { post, inputComments, containerComments, containerReaction, datePostMain };
 }
 
 function createPostHeader(avatarURL) {
-    const postDataUser = document.createElement("div");
-    postDataUser.className = "post-data-user-header";
+  const postDataUser = document.createElement("div");
+  postDataUser.className = "post-data-user-header";
 
-    const containerAvatar = document.createElement("div");
-    containerAvatar.className = "container-avatar-posts";
+  const containerAvatar = document.createElement("div");
+  containerAvatar.className = "container-avatar-posts";
 
-    const imageAvatar = document.createElement("img");
-    imageAvatar.src = avatarURL;
-    imageAvatar.className = "image-avatar";
+  const imageAvatar = document.createElement("img");
+  imageAvatar.src = avatarURL;
+  imageAvatar.className = "image-avatar";
 
-    const containerDate = document.createElement("div");
-    containerDate.className = "container-date";
+  const containerDate = document.createElement("div");
+  containerDate.className = "container-date";
 
-    const namePost = document.createElement("p");
-    namePost.textContent = "Julian Ontiveros";
-    namePost.className = "name-post";
+  const namePost = document.createElement("p");
+  namePost.textContent = "Julian Ontiveros";
+  namePost.className = "name-post";
 
-    const datePostMain = document.createElement("p");
-    datePostMain.className = "date-post-main";
+  const datePostMain = document.createElement("p");
+  datePostMain.className = "date-post-main";
 
-    containerDate.append(namePost, datePostMain);
-    containerAvatar.append(imageAvatar, containerDate);
-    postDataUser.appendChild(containerAvatar);
+  containerDate.append(namePost, datePostMain);
+  containerAvatar.append(imageAvatar, containerDate);
+  postDataUser.appendChild(containerAvatar);
 
-    return { postDataUser, datePostMain };
+  return { postDataUser, datePostMain };
 }
 
 function createSendIcon(containerInput, i) {
-    const icon = document.createElement("i");
-    icon.className = `bi bi-send icon-send-${i}`;
-    containerInput.appendChild(icon);
+  const icon = document.createElement("i");
+  icon.className = `bi bi-send icon-send-${i}`;
+  containerInput.appendChild(icon);
 }
 
 function createReactionBar(containerReaction) {
-    const iconThumb = document.createElement("i");
-    iconThumb.className = "bi bi-hand-thumbs-up-fill icon-color";
+  const divContainerThumb = document.createElement("div");
+  divContainerThumb.className = "div-container-thumb";
+  const iconThumb = document.createElement("i");
+  iconThumb.className = "bi bi-hand-thumbs-up-fill icon-color";
+  const spanLike = document.createElement("span");
+  spanLike.textContent = "0 likes";
+  divContainerThumb.append(iconThumb, spanLike);
 
-    const iconMessage = document.createElement("i");
-    iconMessage.className = "bi bi-chat-left-fill icon-color";
+  const divContainerMessage = document.createElement("div");
+  divContainerMessage.className = "div-container-message";
+  const iconMessage = document.createElement("i");
+  iconMessage.className = "bi bi-chat-left-fill icon-color";
+  const spanMessage = document.createElement("span");
+  spanMessage.textContent = "0 comentarios";
+  divContainerMessage.append(iconMessage, spanMessage);
 
-    const divContainerThumb = document.createElement("div");
-    divContainerThumb.className = "div-container-thumb";
-    const spanLike = document.createElement("span");
-    spanLike.textContent = "30 likes";
-    divContainerThumb.append(iconThumb, spanLike);
-
-    const divContainerMessage = document.createElement("div");
-    divContainerMessage.className = "div-container-message";
-    const spanMessage = document.createElement("span");
-    spanMessage.textContent = "40 Comentarios";
-    divContainerMessage.append(iconMessage, spanMessage);
-
-    containerReaction.append(divContainerThumb, divContainerMessage);
+  containerReaction.append(divContainerThumb, divContainerMessage);
 }
 
 function renderComments(comments, containerComments, avatarURL) {
-    containerComments.innerHTML = "";
-    comments.forEach(element => {
-        const commentContainer = document.createElement("div");
-        commentContainer.className = "comment-container";
+  containerComments.innerHTML = "";
+  comments.forEach(element => {
+    const commentContainer = document.createElement("div");
+    commentContainer.className = "comment-container";
 
-        const avatarDiv = document.createElement("div");
-        avatarDiv.className = "avatar-comments";
-        const imgAvatar = document.createElement("img");
-        imgAvatar.src = avatarURL;
-        imgAvatar.className = "img-avatar";
-        avatarDiv.appendChild(imgAvatar);
+    const avatarDiv = document.createElement("div");
+    avatarDiv.className = "avatar-comments";
+    const imgAvatar = document.createElement("img");
+    imgAvatar.src = avatarURL;
+    imgAvatar.className = "img-avatar";
+    avatarDiv.appendChild(imgAvatar);
 
-        const textContainer = document.createElement("div");
-        textContainer.className = "container-text-post";
-        const textPost = document.createElement("p");
-        textPost.textContent = element.texto;
-        const datePost = document.createElement("p");
-        datePost.className = "date-post";
+    const textContainer = document.createElement("div");
+    textContainer.className = "container-text-post";
+    const textPost = document.createElement("p");
+    textPost.textContent = element.texto;
+    const datePost = document.createElement("p");
+    datePost.className = "date-post";
 
-        textContainer.append(textPost, datePost);
-        commentContainer.append(avatarDiv, textContainer);
-        containerComments.appendChild(commentContainer);
+    textContainer.append(textPost, datePost);
+    commentContainer.append(avatarDiv, textContainer);
+    containerComments.appendChild(commentContainer);
 
-        showingComments(comments.length > 0, containerComments);
-        formatDateComments(datePost, element);
-    });
+    formatDateComments(datePost, element);
+    containerComments.style.display = comments.length ? "block" : "none";
+
+  });
+}
+
+function updateCommentCount(containerReaction, count) {
+  const spanMessage = containerReaction.querySelector(".div-container-message span");
+  spanMessage.textContent = `${count} comentarios`;
 }
 
 function formatDate(datePost, element) {
-    const timestamp = element.fechaCreacion;
-    datePost.textContent = new Date(timestamp.seconds * 1000).toLocaleDateString("es-ES", {
-        day: "numeric",
-        month: "long",
-        year: "numeric"
-    });
+  const timestamp = element.fechaCreacion;
+  datePost.textContent = new Date(timestamp.seconds * 1000).toLocaleDateString("es-ES", {
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+  });
 }
 
 function formatDateComments(datePost, element) {
-    const timestamp = element.fecha;
-    datePost.textContent = new Date(timestamp.seconds * 1000).toLocaleDateString("es-ES", {
-        day: "numeric",
-        month: "long",
-        year: "numeric"
-    });
-}
-
-function showingComments(show, containerComments) {
-    containerComments.style.display = show ? "block" : "none";
+  const timestamp = element.fecha;
+  datePost.textContent = new Date(timestamp.seconds * 1000).toLocaleDateString("es-ES", {
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+  });
 }
--- listeners/listeners.js
+++ listeners/listeners.js
@@ -1,21 +1,48 @@
-import { addComments} from "../queriesFirebase.js";
+import { addComments, addLike, removeLike, getLikes } from "../queriesFirebase.js";
 
-export function carruselListeners(btn, container, direction, itemWidth) {
-  btn.addEventListener("click", (e) => {
-    container.scrollLeft += itemWidth * direction
-  })
-}
+// Enviar comentario
+export function sendComment(iconSend, containerComments, inputComments, postId) {
+  iconSend.addEventListener("click", async () => {
+    const texto = inputComments.value.trim();
+    if (!texto) return;
 
-export function sendComment(
-  iconSend,
-  containerComments,
-  inputComments,
-  idPostComments
-) {
-  iconSend.addEventListener("click", (e) => {
+    await addComments(texto, postId);
+    inputComments.value = "";
     containerComments.style.display = "block";
-    addComments(inputComments.value, idPostComments);
-  })
+  });
 }
 
+// Likes
+export async function likePost(iconThumb, spanLike, postId, userId = "Anon") {
+  let liked = false;
 
+  // Al cargar, revisa si el usuario ya dio like
+  const likes = await getLikes(postId);
+  const count = likes.length;
+  spanLike.textContent = `${count} likes`;
+  if (likes.includes(userId)) {
+    liked = true;
+    iconThumb.style.color = "#1877F2";
+  }
+
+  iconThumb.addEventListener("click", async () => {
+    if (!liked) {
+      await addLike(postId, userId);
+      spanLike.textContent = `${parseInt(spanLike.textContent) + 1} likes`;
+      iconThumb.style.color = "#1877F2";
+      liked = true;
+    } else {
+      await removeLike(postId, userId);
+      spanLike.textContent = `${parseInt(spanLike.textContent) - 1} likes`;
+      iconThumb.style.color = "";
+      liked = false;
+    }
+  });
+}
+
+// Carrusel (opcional)
+export function carruselListeners(btn, container, direction, itemWidth) {
+  btn.addEventListener("click", () => {
+    container.scrollLeft += itemWidth * direction;
+  });
+}
